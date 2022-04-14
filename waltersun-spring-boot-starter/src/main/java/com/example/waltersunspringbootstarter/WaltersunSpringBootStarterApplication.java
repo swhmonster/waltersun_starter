c5cb0656507f957b560467e2aac3d266be287c5e
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2022-04-14 20:08:57 +0800

    【+】stater create

---
+++ waltersun-spring-boot-starter/src/main/java/com/example/waltersunspringbootstarter/WaltersunSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.waltersunspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaltersunSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaltersunSpringBootStarterApplication.class, args);
+    }
+
+}
